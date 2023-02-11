5ffcb317507b43e724205ffd08956f25520aad46
Miras Adilov <miras9191@gmail.com>
2023-02-10 21:44:27 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,23 +1,75 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const db = require('../db/queries.js')
-const port = 3000
+const express = require('express');
+const compression = require('compression');
 
-const path = require('path');
-const LOADER_PATH = path.resolve(__dirname + '/loaderio-a205387990b22005671fb6b52113d6a5.txt');
-app.use(express.json())
-app.use('/loaderio-a205387990b22005671fb6b52113d6a5.txt', express.static(LOADER_PATH));
+const app = express();
+const cors = require('cors');
+let port = process.env.PORT || 3000;
 
-app.get('/api/products', db.getAllProducts)
-app.get('/api/products/:id/related', db.getRelated)
-app.get('/api/products/:id', db.getProduct)
-//combination of features
-app.get('/api/products/:id/styles', db.getStyles)
-//combination of photos/skus
+const cluster = require('cluster');
+const totalCPUs = require('os').cpus().length;
 
+if (cluster.isMaster) {
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    cluster.fork();
+  })
+} else {
 
+  //Import Routes
+  const productRouter = require('./controller/productRoutes');
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+  //Middleware Setup
+  app.use(compression())
+  app.use('/products', productRouter);
+  app.use(express.json());
+  app.use(cors());
+
+  //Basic Routes for Testing
+  app.get('/hello-world', (req, res) => {
+    res.json({ message: 'Hello World!' })
+  });
+
+  let htmlMessage = `<h1>Welcome!</h1><p>You have reached the HR-SDC products API. To consume this REST API, please visit one of the following links:</p>
+  <ul>
+    <li>
+    <a href="/products">Products</a>
+    </li>
+    <li>
+    <a href="/products/1">A Specific Product (#1)</a>
+    </li>
+    <li>
+    <a href="/products/1/related">Related Products</a>
+    </li>
+    <li>
+    <a href="/products/1/styles">Product Styles</a>
+    </li>
+  </ul>`;
+
+  //Welcome Page
+  app.get('/', (req, res) => {
+    res.setHeader('Content-type', 'text/html');
+    res.status(200).send(htmlMessage);
+  });
+
+  app.get('/loaderio-b4d0ac2c41481e9702d38f174f5095d5/', (req, res) => {
+    res.status(200).send('loaderio-b4d0ac2c41481e9702d38f174f5095d5');
+  });
+
+  //Not Found Middleware
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  app.listen(port, (err) => {
+    err ?
+      console.log(error) :
+      console.log("Server listening on port:", port);
+  });
+
+}
+
+module.exports = { app, port }
